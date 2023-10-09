e5f87652862ddb770b0a912234e1589699d394c5
jocaher0503 <jocaher0503@g.educaand.es>
2023-10-09 10:27:37 +0200

    09/10/2023

---
+++ Desktop/1ºDAM/Programación/JavaProjects/CAP03/src/CAP03EJ09.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class CAP03EJ09 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Introduzca la longitud de la altura del cono (cm): ");
+        double altura=sc.nextDouble();
+        System.out.print("Introduzca el radio del cono (cm): ");
+        double radio=sc.nextDouble();
+    }
+}
