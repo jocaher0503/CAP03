dc00ce9a5c8771fa3d0b93d0e859de9c495ed03e
jocaher0503 <jocaher0503@g.educaand.es>
2023-10-09 10:24:20 +0200

    09/10/2023

--- src/CAP03EJ08.java
+++ src/CAP03EJ08.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class CAP03EJ08 {
     public static void main(String[] args) {
-        
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Por favor, introduzca la cantidad de horas semanales que trabaja: ");
+        int horas= sc.nextInt();
+        System.out.print("Su salario semanal es de: "+(horas*12)+" euros");
+        sc.close();
     }
 }
