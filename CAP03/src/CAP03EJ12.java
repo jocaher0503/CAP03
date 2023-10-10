cd2c7034bcd67c2ce86894fe4897c6a04b5022e0
jocaher0503 <jocaher0503@g.educaand.es>
2023-10-10 08:21:40 +0200

    10/10/2023

--- CAP03/src/CAP03EJ12.java
+++ CAP03/src/CAP03EJ12.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class CAP03EJ12 {
     public static void main(String[] args) {
-        
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Introduzca la nota del primer examen: ");
+        double nota1= sc.nextDouble();
+        System.out.print("¿Qué nota quiere sacar en el trimestre? ");
+        double notaFinal = sc.nextDouble();
+        double nota2 = ((notaFinal * 100) - (nota1 * 40)) / 60;
+        System.out.print("Para tener un " + notaFinal + " en el trimestre");
+        System.out.println(" necesita sacar un " + nota2 + " en el segundo examen.");  
     }
 }
